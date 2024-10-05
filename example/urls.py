e4ef297bb67c90c32a01ae3033590598c314d529
globalsjconsultant <globalsjconsultant@gmail.com>
2024-10-05 17:37:00 +0500

    domain added

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,31 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'example'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+
+    path('team', views.team, name='team'),
+    path('team_detail', views.team_detail, name='team_detail'),
+
+    path('service', views.service, name='service'),
+    path('service_detail', views.service_detail, name='service_detail'),
+
+    path('blog', views.blog, name='blog'),
+    path('blog_detail', views.blog_detail, name='blog_detail'),
+
+
+    path('coaching', views.coaching, name='coaching'),
+    path('coaching_detail', views.coaching_detail, name='coaching_detail'),
+
+    path('country', views.country, name='country'),
+    path('country_detail', views.country_detail, name='country_detail'),
+
+    path('404', views.notfound, name='404'),
+    path('faq', views.faq, name='faq'),
+    path('service', views.service, name='service'),
+    path('contact', views.contact, name='contact')
 
 ]
--- example/views.py
+++ example/views.py
@@ -16,3 +16,35 @@ def index(request):
     </html>
     '''
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+def team(request):
+    return render(request, 'team.html')
+def team_detail(request):
+    return render(request, 'team_detail.html')
+def notfound(request):
+    return render(request, 'notfound.html')
+def faq(request):
+    return render(request, 'faq.html')
+def service(request):
+    return render(request, 'service.html')
+def service_detail(request):
+    return render(request, 'service_detail.html')
+
+def coaching(request):
+    return render(request, 'coaching.html')
+def coaching_detail(request):
+    return render(request, 'coaching_detail.html')
+def country(request):
+    return render(request, 'country.html')
+def country_detail(request):
+    return render(request, 'country_detail.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+def blog_detail(request):
+    return render(request, 'blog_detail.html')
+
+def contact(request):
+    return render(request, 'contact.html')
