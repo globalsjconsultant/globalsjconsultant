67951a887cb064d140ecf7a2f9e7e49f2ba3ef4d
globalsjconsultant <globalsjconsultant@gmail.com>
2024-10-06 12:25:30 +0500

    domain added

--- api/settings.py
+++ api/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='globalsjconsultant.com', cast=Csv())
 
 
 
